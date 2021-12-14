8b7c84e814a2c2b0bf62e1c7892869cc5a2167b0
serxka <serxka@protonmail.com>
2021-12-14 14:49:21 +1100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate pros;
+use pros::prelude::*;
+
+struct VexRobot;
+
+impl Robot for VexRobot {
+	fn new() -> Self {
+		VexRobot
+	}
+
+	fn competition_init(&mut self) {
+		println!("competition_init()");
+	}
+
+	fn disabled(&mut self) {
+		println!("disabled()");
+	}
+
+	fn autonomous(&mut self) {
+		println!("autonomous()");
+	}
+
+	fn opcontrol(&mut self) {
+		println!("opcontrol()");
+	}
+}
+
+robot!(VexRobot);
